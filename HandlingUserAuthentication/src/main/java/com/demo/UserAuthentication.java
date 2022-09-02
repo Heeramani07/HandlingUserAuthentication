53194e1d6347cda6df4aa47ebe60be16364b806d
USER <USER@DESKTOP-S69L5QC>
2022-09-02 14:53:23 +0530

    Handling commit

---
+++ HandlingUserAuthentication/src/main/java/com/demo/UserAuthentication.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+public class UserAuthentication {
+	public String getUserName() {
+		return "Heeramani";
+	}
+	public String getEmail() {
+		return "Heera@gmail.com";
+	}
+	public int getPhnNum() {
+		return 987654321;
+	}
+	public String getPassword() {
+		return "Mani@123";
+	}
+
+}
---
+++ HandlingUserAuthentication/src/test/java/com/demo/UserAuthenticationTest.java
@@ -0,0 +1,39 @@
+package com.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserAuthenticationTest {
+
+	private UserAuthentication userA;
+	@BeforeEach
+	public void setup() {
+		userA=new UserAuthentication();
+		System.out.println("Object initialized");
+	}
+	@Test
+	public void verifyUserName() {
+		assertEquals(userA.getUserName(), "Heeramani");
+	}
+	@Test
+	public void verifyEmail() {
+		assertEquals(userA.getEmail(), "Heera@gmail.com");
+	}
+	@Test
+	public void verifyPhnNum() {
+		assertEquals(userA.getPhnNum(), 987654321);
+	}
+	@Test
+	public void verifyPassword() {
+		assertEquals(userA.getPassword(), "Mani@123");
+	}
+	@AfterEach
+	public void tearDown() {
+		userA=null;
+		System.out.println("Object destroyed");
+	}
+
+}
